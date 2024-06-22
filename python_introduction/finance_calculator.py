ff0ca1d79bd6d8497e99526357743963e9edf9e9
osasenaga <edionwe.osasenaga5@gmail.com>
2024-06-22 22:25:05 +0100

    calculation of age

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,6 @@
+monthly_income = int(input("Enter your monthly income: "))
+total_monthly_expenses = int(input("Enter your total monthly expenses: "))
+Monthly_Savings = total_monthly_expenses - monthly_income
+print("Your monthly savings are", "$",Monthly_Savings)
+Projected_Savings = Monthly_Savings * 12 + Monthly_Savings * 12 * 0.05
+print("Projected savings after one year,", "with interest,", "is:", "$", Projected_Savings)
