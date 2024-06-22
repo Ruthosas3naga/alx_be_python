19cdc22a91f13dab9ef9e6bf6d5b2406655faddf
osasenaga <edionwe.osasenaga5@gmail.com>
2024-06-22 18:23:31 +0100

    addition, subtraction and multiplication of numbers

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -2,3 +2,13 @@ number1 = 10
 number2 = 5
 result = number1 + number2
 print("addition of", number1, "and", number2, "is", result)
+
+number1 = 10
+number2 = 5
+result = number1 - number2
+print("subtraction of", number2, "and", number1, "is", result)
+
+number1 = 10
+number2 = 5
+result = number1 * number2
+print("multiplication of", number1, "and", number2, "is", result)
