f99c8a783d804f48992cfabcfdbb448975486f5a
osasenaga <edionwe.osasenaga5@gmail.com>
2024-07-20 23:34:07 +0100

    library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -21,12 +21,12 @@ class EBook(Book):
 
     
 class PrintBook(Book):
-    def __init__(self,title:str, author:str,page_count:int):
+    def __init__(self,title:str, author:str, page_count:int):
         super().__init__(title,author)
         self.page_count = page_count
 
     def __str__(self):
-        return f"PrintBook: {self.title} by {self.author}, Page count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
     
   
     
