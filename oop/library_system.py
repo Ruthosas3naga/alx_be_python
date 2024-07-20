83d9d28e2282b8ebdbe58de78d83047c28205d0e
osasenaga <edionwe.osasenaga5@gmail.com>
2024-07-20 22:46:43 +0100

    library system

---
+++ oop/library_system.py
@@ -0,0 +1,28 @@
+class Book:
+    def __init__(self,title:str, author:str):
+     self.title = title
+     self.author = author
+
+class Ebook:
+   def __init__(self,title:str, author:str,file_size:int):
+    super().__init__(title,author)
+    self.file_size = file_size
+
+class PrintBook:
+    def __init__(self,title:str, author:str,page_count:int):
+        super().__init__(title,author)
+        self.page_count = page_count
+
+class Library:
+   def __init__(self):
+      self.books = []
+
+   def add_book(self, book):
+      self.books.append(book)
+
+   def list_books(self):
+      print(self.list_books)
+      
+      
+
+       
