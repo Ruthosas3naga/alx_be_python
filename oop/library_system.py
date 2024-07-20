b9c9b0edbd51f2e7bdeb5f75f50fe3bc463c71bc
osasenaga <edionwe.osasenaga5@gmail.com>
2024-07-20 23:09:50 +0100

    library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -41,9 +41,12 @@ class Library:
    def add_book(self, book):
       self.books.append(book)
 
-   def list_books(self):
-      for book in self.books:
-        print(self.books)
+   def add_book(self, book):
+        if isinstance(book, Book):
+            self.books.append(book)
+            print(f"Added {book}")
+        else:
+            raise ValueError("Can only add instances of Book, EBook, or PrintBook")
 
    def __str__(self):
         return f"Library with {len(self.books)} books"
