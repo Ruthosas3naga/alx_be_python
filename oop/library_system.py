bfacc90405779a0db887d49737e8aebd62991138
osasenaga <edionwe.osasenaga5@gmail.com>
2024-07-20 23:02:53 +0100

    library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,15 +3,36 @@ class Book:
      self.title = title
      self.author = author
 
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}')"
+
+
 class EBook(Book):
    def __init__(self,title:str, author:str,file_size:int):
     super().__init__(title,author)
     self.file_size = file_size
 
+   def __str__(self):
+        return f"{self.title} by {self.author} {self.file_size}"
+    
+   def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', '{self.file_size}')"
+
+    
 class PrintBook(Book):
     def __init__(self,title:str, author:str,page_count:int):
         super().__init__(title,author)
         self.page_count = page_count
+
+    def __str__(self):
+        return f"{self.title} by {self.author} {self.page_count}"
+    
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', '{self.page_count}')"
+    
 
 class Library:
    def __init__(self):
@@ -21,8 +42,7 @@ class Library:
       self.books.append(book)
 
    def list_books(self):
-      print(self.list_books)
-      
-      
+      for book in self.books:
+        print(self.books)
 
-       
+   
